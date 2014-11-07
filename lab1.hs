9c138c7b06029cf742407cf235f601f0b6cca6c3
Tobias Tikka <tobias.tikka@gmail.com>
2014-11-07 14:08:30 +0100

    fixes

--- lab1.hs
+++ lab1.hs
@@ -3,8 +3,8 @@ import Test.QuickCheck
 {-
 
 Lab assignment: 1
-By: 			Tobias Tikka & Marcus Olsson
-Course: 		TDA452
+By:             Tobias Tikka & Marcus Olsson
+Course:         TDA452
 
 -}
 
@@ -24,32 +24,33 @@ It takes k+1 steps to calculate the function. (O(k))
 -- PART 2:
 
 power1 :: Int -> Int -> Int
-power1 n k | k == 0		= 1
-	 	   | k < 0 		= error "power: negative argument"
-		   | otherwise 	= product (replicate k n) 
-		   			  -- (take k (repeat n)) would work as well
+power1 n k 
+            | k < 0         = error "power: negative argument"
+            | otherwise     = product (replicate k n) 
+                      -- (take k (repeat n)) would work as well
 
 -- PART 3:
 
 power2 :: Int -> Int -> Int
-power2 n k | k == 0		= 1
-	       | k < 0 		= error "power: negative argument"
-		   | odd k		= n * power2 n (k-1)
-		   | even k 	= power2 (n * n) (div k 2)
+power2 n k 
+            | k == 0        = 1
+            | k < 0         = error "power: negative argument"
+            | odd k         = n * power2 n (k-1)
+            | even k        = power2 (n * n) (k `div` 2)
 
 --PART 4:
 {- 
 
 A:
 
-n = 5, k = 3	== 125
-n = 3, k = 5	== 243
-n = 0, k = 5	== 0
-n = 5, k = 0	== 1
-n = 5, k = -1	== error	
+n = 5, k = 3    == 125
+n = 3, k = 5    == 243
+n = 0, k = 5    == 0
+n = 5, k = 0    == 1
+n = 5, k = -1   == error    
 (According to information in the Google Group we shouldn't test negatives)
-n = -2, k = 3 	== -8
-n = -2, k = 2 	== 4
+n = -2, k = 3   == -8
+n = -2, k = 2   == 4
 
 -}
 
@@ -57,20 +58,24 @@ n = -2, k = 2 	== 4
 
 prop_powers :: Int -> Int -> Bool
 prop_powers n k = (power n k) == (power1 n k) 
-				&& (power1 n k) == (power2 n k)
+                && (power1 n k) == (power2 n k)
 
 -- C:
 
-test_powers :: Bool 
-test_powers =	(prop_powers 5 3) &&
-				(prop_powers 3 5) &&
-				(prop_powers 0 5) &&
-				(prop_powers 5 0) &&
-				(prop_powers (-2) 3) &&
-				(prop_powers (-2) 2)
+-- We write a function that puts an element in a list if
+-- a test case fails. Returns True if the array is 0, .e.g. 
+-- all tests OK and False otherwise 
+
+test_powers :: Bool
+test_powers = length ["Test case failed"
+                | n <- [5, 3, 0, 5, (-2), (-2)], 
+                  k <- [3, 5, 5, 0, 3, 2],
+                  prop_powers n k == False] == 0
+
+
+-- We reuse the prop_powers function and always makes sure that k is positive.
 
 prop_powers' :: Int -> Int -> Bool
-prop_powers' n k | k < 0 = True
-				 | otherwise = (power n k) == (power1 n k) 
-				   && (power1 n k) == (power2 n k)
-
+prop_powers' n k    
+                    | k < 0 = prop_powers n (-k)
+                    | otherwise = prop_powers n k
