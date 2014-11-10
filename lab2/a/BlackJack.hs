2212d8d686affc779c12c0d91eb8c0d42eb6bbb1
Marcus Olsson <marcus@meeloo.se>
2014-11-10 12:02:37 +0100

    Correct value function

--- lab2/a/BlackJack.hs
+++ lab2/a/BlackJack.hs
@@ -2,7 +2,14 @@ module BlackJack where
 import Cards
 import Wrapper
 
--- Todo: Describe what happens in the size function.
+{-
+	#1  (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty))
+	#2  1 + (Add (Card Jack Spades) Empty)
+	#3  1 + 1 + Empty
+	#4  1 + 1 + 0
+	#5	== 2
+-}
+
 -- Todo: Document and prop_testing
 
 empty :: Hand
@@ -21,17 +28,20 @@ numberOfAces Empty = 0
 numberOfAces (Add (Card r _) h) | r == Ace      = 1 + numberOfAces h
                                 | otherwise     = numberOfAces h
 
--- Todo: Fix for aces when value > 21. Calculate total value of hand and remove 10 * numberOfAces if value > 21? 
+valueHelper :: Hand -> Integer
+valueHelper Empty = 0
+valueHelper (Add c h) = valueCard c + valueHelper h
+
 value :: Hand -> Integer
-value Empty = 0
-value (Add c h) = valueCard c + value h
+value h | valueHelper h > 21 = valueHelper h - (numberOfAces h * 10)
+		| otherwise = valueHelper h
 
 gameOver :: Hand -> Bool
 gameOver h  | value h > 21 = True
             | otherwise = False
 
 winner :: Hand -> Hand -> Player
-winner g b  | gameOver g == True = Bank
-            | gameOver b == True = Guest
-            | value b >= value g = Bank
-            | otherwise = Guest
+winner g b  | gameOver g 			= Bank
+            | gameOver b 			= Guest
+            | value b >= value g 	= Bank
+            | otherwise 			= Guest
