fc751cb7f1a46b6c11487d1808c3b16b53559cf5
Tobias Tikka <tobias.tikka@gmail.com>
2014-11-11 09:09:19 +0100

    Added comments to Lab2, part A

--- lab2/a/BlackJack.hs
+++ lab2/a/BlackJack.hs
@@ -10,34 +10,47 @@ import Wrapper
 	#5	== 2
 -}
 
--- Todo: Document and prop_testing
-
+-- We define a "variable" empty that returns an empty hand
 empty :: Hand
 empty = Empty
 
+{-
+The valueRank function checks the value of a specific rank that you give it.
+We have added a guard for the numeric values that only accepts values between 2
+and 10. We do however believe that it would be better to have this check when 
+you create a Card.
+-}
 valueRank :: Rank -> Integer
-valueRank (Numeric n)   = n
-valueRank Ace           = 11
-valueRank _             = 10
+valueRank (Numeric n) | 1 < n && n <= 10	= n
+valueRank Ace           					= 11
+valueRank _             					= 10
 
+-- We only care about the rank when we evaluate the value of a Card, we therefore
+-- call for the valueRank function
 valueCard :: Card -> Integer
 valueCard (Card r _) = valueRank r
 
+-- Recursively count the number of aces in the Hand, stop when the hand is Empty
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add (Card r _) h) | r == Ace      = 1 + numberOfAces h
                                 | otherwise     = numberOfAces h
 
-
+{-
+Count the value of the Hand, take in consideration that the Ace is evaluated to 11
+if n =< 21 and 1 if n > 21, where n is the sum of all ranks
+-}
 value :: Hand -> Integer
 value h | valueHelper h > 21 = valueHelper h - (numberOfAces h * 10)
 		| otherwise = valueHelper h
 	where
+		-- Helper function to sum all the ranks of the cards
 		valueHelper :: Hand -> Integer
 		valueHelper Empty = 0
 		valueHelper (Add c h) = valueCard c + valueHelper h
 
-
+-- Self-explanatory; stop game if the value of the hand that you gives the function
+-- exceeds 21
 gameOver :: Hand -> Bool
 gameOver h  | value h > 21 = True
             | otherwise = False
