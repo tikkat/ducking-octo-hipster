075058413e38656d9d719657fd12c60f50eb7abd
Marcus Olsson <marcus@meeloo.se>
2014-11-19 10:30:14 +0100

    Hello

--- lab3/b/Sudoku.hs
+++ lab3/b/Sudoku.hs
@@ -9,6 +9,30 @@ import Test.QuickCheck
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
 	deriving (Show, Eq)
 
+example :: Sudoku
+example = Sudoku [
+	[Just 3, Just 6, Nothing,Nothing,Just 7, Just 1, Just 2, Nothing,Nothing],
+	[Nothing,Just 5, Nothing,Nothing,Nothing,Nothing,Just 1, Just 8, Nothing],
+	[Nothing,Nothing,Just 9, Just 2, Nothing,Just 4, Just 7, Nothing,Nothing],
+	[Nothing,Nothing,Nothing,Nothing,Just 1, Just 3, Nothing,Just 2, Just 8],
+	[Just 4, Nothing,Nothing,Just 5, Nothing,Just 2, Nothing,Nothing,Just 9],
+	[Just 2, Just 7, Nothing,Just 4, Just 6, Nothing,Nothing,Nothing,Nothing],
+	[Nothing,Nothing,Just 5, Just 3, Nothing,Just 8, Just 9, Nothing,Nothing],
+	[Nothing,Just 8, Just 3, Nothing,Nothing,Nothing,Nothing,Just 6, Nothing],
+	[Nothing,Nothing,Just 7, Just 6, Just 9, Nothing,Nothing,Just 4, Just 3]]
+
+example2 :: Sudoku
+example2 = Sudoku [
+	[Just 1, Just 2, Just 3, Just 4, Just 5, Just 6, Just 7, Just 8, Just 9],
+	[Just 10, Just 11, Just 12, Just 13, Just 14, Just 15, Just 16, Just 17, Just 18],
+	[Just 19, Just 20, Just 21, Just 22, Just 23, Just 24, Just 25, Just 26, Just 27],
+	[Just 28, Just 29, Just 30, Just 31, Just 32, Just 33, Just 34, Just 35, Just 36],
+	[Just 37, Just 38, Just 39, Just 40, Just 41, Just 42, Just 43, Just 44, Just 45],
+	[Just 46, Just 47, Just 48, Just 49, Just 50, Just 51, Just 52, Just 53, Just 54],
+	[Just 55, Just 56, Just 57, Just 58, Just 59, Just 60, Just 61, Just 62, Just 63],
+	[Just 64, Just 65, Just 66, Just 67, Just 68, Just 69, Just 70, Just 71, Just 72],
+	[Just 73, Just 74, Just 75, Just 76, Just 77, Just 78, Just 79, Just 80, Just 81]]
+
 -- allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku $ replicate 9 $ replicate 9 Nothing
@@ -32,19 +56,16 @@ printSudoku s = putStrLn $ unlines [[if isNothing column then '.' else
 readSudoku :: FilePath -> IO Sudoku
 readSudoku f =
 	do
-		s <- getLines f
+		content <- readFile f
+		let s = Sudoku [[if a == '.' then Nothing else 
+				Just (ord a - 48) | a <- k] | k <- lines content]
+
 		if isSudoku s
 			then
 				return s
 			else do
 				putStrLn "Program error: Not a valid Sudoku!"
 				return allBlankSudoku
-	where
-		getLines :: FilePath -> IO Sudoku
-		getLines f = do
-			content <- readFile f
-			return	(Sudoku [[if a == '.' then Nothing else 
-					Just (ord a - 48) | a <- k] | k <- lines content])
 
 -- cell generates an arbitrary cell in a Sudoku
 cell :: Gen (Maybe Int)
@@ -67,3 +88,10 @@ type Block = [Maybe Int]
 isOkayBlock :: Block -> Bool
 isOkayBlock b = length b == 9 && length a == length (nub a)
 	where a = [k | k <- b, isJust k]
+
+blocks :: Sudoku -> [Block]
+blocks s = undefined; --(rows s):(rows (transpose s))
+
+{-threeByThreeBlocks :: [[Maybe Int]] -> [Block]
+threeByThreeBlocks rs = [take 9 $ drop n ungroupped | n <- [0,9..72]]
+  where ungroupped = concat [take 3 $ drop n (rs !! i) | n <- [0,3,6], i <- [0..8]]-}
