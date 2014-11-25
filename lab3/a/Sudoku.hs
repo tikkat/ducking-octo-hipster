f8b534cb556c0121b062510c4cc2dbcb662c30b1
Tobias Tikka <tobias.tikka@gmail.com>
2014-11-25 10:21:33 +0100

    added some shit

--- lab3/a/Sudoku.hs
+++ lab3/a/Sudoku.hs
@@ -1,83 +1,49 @@
 module Sudoku where
 
-import Data.Maybe
-import Data.Char
-import Data.List
-
 import Test.QuickCheck
+import Data.Maybe
+
+-------------------------------------------------------------------------
 
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
-  deriving (Show, Eq)
+ deriving ( Show, Eq )
 
 -- allBlankSudoku is a sudoku with just blanks
 allBlankSudoku :: Sudoku
 allBlankSudoku = Sudoku $ replicate 9 $ replicate 9 Nothing
 
--- isSudoku sud checks if sud is really a valid representation of a sudoku puzzle,
--- i.e. 9 x 9 digits in the range 1 - 9 (or nothing)
+-- isSudoku sud checks if sud is really a valid representation of a sudoku
+-- puzzle
 isSudoku :: Sudoku -> Bool
-isSudoku s = length (rows s) == 9 && 
-  and [length row == 9 && and [isNothing column || 
-  (fromJust column > 0 && fromJust column < 10) | column <- row] | row <- rows s]
+isSudoku s  = length ( rows s ) /= 9 &&
+             and [length tmp == 9 | tmp <- [rows s]] &&
+             and[and[isNothing b || (fromJust b > 0 && fromJust b < 10) | b <- a] | a <- rows s]
 
 -- isSolved sud checks if sud is already solved, i.e. there are no blanks
 isSolved :: Sudoku -> Bool
-isSolved s = and [and [isJust column | column <- row] | row <- rows s]
+isSolved = undefined
+
+-------------------------------------------------------------------------
 
 -- printSudoku sud prints a representation of the sudoku sud on the screen
 printSudoku :: Sudoku -> IO ()
-printSudoku s = putStrLn $ unlines [[if isNothing column then '.' else 
-        chr $ 48 + fromJust column | column <- row] | row <- rows s]
+printSudoku = undefined
 
--- readSudoku file reads from the file, and either delivers it, 
--- or stops if the file did not contain a sudoku
+-- readSudoku file reads from the file, and either delivers it, or stops
+-- if the file did not contain a sudoku
 readSudoku :: FilePath -> IO Sudoku
-readSudoku f =
-  do
-    content <- readFile f
-    let s = Sudoku [[if a == '.' then Nothing else Just (ord a - 48) | a <- k] 
-                   | k <- lines content]
+readSudoku = undefined
 
-    if isSudoku s then return s else do
-      putStrLn "Program error: Not a valid Sudoku!"
-      return allBlankSudoku
+-------------------------------------------------------------------------
 
 -- cell generates an arbitrary cell in a Sudoku
 cell :: Gen (Maybe Int)
-cell =
-  do
-    n <- choose (1, 9)
-    frequency [(9, return Nothing), (1, return $ Just n)]
+cell = undefined
 
 -- an instance for generating Arbitrary Sudokus
 instance Arbitrary Sudoku where
   arbitrary =
-    do
-      rows <- sequence [sequence [cell | j <- [1..9]] | i <- [1..9]]
-      return (Sudoku rows)
+    do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
+       return (Sudoku rows)
 
-prop_Sudoku :: Sudoku -> Bool
-prop_Sudoku = isSudoku
-
-type Block = [Maybe Int]
-
-isOkayBlock :: Block -> Bool
-isOkayBlock b = length b == 9 && length onlyDigits == length (nub onlyDigits)
-  where onlyDigits = filter isJust b
-
-blocks :: Sudoku -> [Block]
-blocks s = rs ++ transpose rs ++ threeByThreeBlocks
-  where
-    rs                    = rows s
-    threeByThreeBlocks    = [take 9 $ drop n inRightOrder | n <- [0, 9..72]]
-      where inRightOrder  = concat [take 3 $ drop n row | n <- [0, 3, 6], 
-                            row <- transpose rs]
-
-prop_blocks :: Sudoku -> Bool
-prop_blocks s = length bs == 27 && and [length block == 9 | block <- bs]
-  where bs = blocks s
-
-isOkay :: Sudoku -> Bool
-isOkay s = and [isOkayBlock block | block <- blocks s]
-
-type Pos = (Int, Int)
+-------------------------------------------------------------------------
