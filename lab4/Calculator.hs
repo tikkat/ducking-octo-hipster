d1ab8495b2230a120a12d63b9a5d13449d8c327c
Marcus Olsson <marcus@meeloo.se>
2014-12-08 17:23:42 +0100

    New better looking graph page

--- lab4/Calculator.hs
+++ lab4/Calculator.hs
@@ -1,65 +1,123 @@
 import Control.Monad (when)
 
 import Haste hiding (eval)
-import Haste.Graphics.Canvas
+import Haste.Prim
+import Haste.Foreign
+import Haste.Graphics.Canvas hiding (scale)
 
 import Data.Maybe
 
 import Expr
-import Pages
 
 canWidth  = 300
 canHeight = 300
 
+scale = 0.04
+
 readAndDraw :: Elem -> Elem -> Canvas -> IO ()
-readAndDraw expInput scaInput can = do
-  exp <- getProp expInput "value"
-  sca <- getProp scaInput "value"
-  let exp' = readExpr exp
-  let allPoints = if isJust exp' then points (fromJust exp') (0.04 / read sca) (canWidth, canHeight) else []
+readAndDraw expInput scaleInput can = do
+  exp           <- getProp expInput "value"
+  inputScale    <- getProp scaleInput "value"
+  let exp'      = readExpr exp
+  let allPoints = if isJust exp' then points (fromJust exp') (scale / read inputScale) (canWidth, canHeight) else []
   render can $ stroke $ path allPoints
 
 readAndDifferentiate :: Elem -> Elem -> Canvas -> IO ()
-readAndDifferentiate expInput scaInput can = do
+readAndDifferentiate expInput scaleInput can = do
   exp <- getProp expInput "value"
   let exp' = readExpr exp
 
   when (isJust exp') $ do
     setProp expInput "value" (showExpr (differentiate (fromJust exp')))
-    readAndDraw expInput scaInput can
+    readAndDraw expInput scaleInput can
 
 main = do
-  -- Elements
-  canvas    <- mkCanvas canWidth canHeight      -- The drawing area
-  fx        <- mkHTML "<i>f</i>(<i>x</i>) = "   -- The text "f(x) = "
-  expElem   <- mkInput 20 "x"                   -- The formula input
-  scaTxt    <- mkHTML "Scale: "                 -- The text "Scale: "
-  scaElem   <- mkInput 10 "1"                   -- The scale input
-  drawBtn   <- mkButton "Draw graph"            -- The draw button
-  diffBtn   <- mkButton "Differentiate"         -- The differentiate button
-  br        <- mkHTML "<br />"                  -- A line break
+  setStyle documentBody "padding" "50px"
+  setStyle documentBody "background-color" "#efefef"
+  setStyle documentBody "text-align" "center"
 
-  -- Layout
-  formula <- mkDiv
-  formula' <- mkDiv
-  formula'' <- mkDiv
-  row formula [fx, expElem]
-  row formula' [scaTxt, scaElem]
-  column documentBody [canvas, formula, formula', br, diffBtn, drawBtn]
+  canvas <- newCanvas canWidth canHeight
 
-  -- Styling
-  setStyle documentBody "backgroundColor" "lightblue"
-  setStyle documentBody "textAlign" "center"
-  setStyle expElem "fontSize" "14pt"
-  setStyle scaElem "fontSize" "14pt"
-  setStyle diffBtn "fontSize" "11pt"
-  setStyle drawBtn "fontSize" "11pt"
+  (expElem, expInput) <- newInput "x" "f(x) ="
+  (scaleElem, scaleInput) <- newInput "1" "Scale:"
 
-  focus expElem
-  select expElem
+  drawButton <- newButton "Draw"
+  diffButton <- newButton "Differentiate"
 
-  -- Interaction
+  buttonDiv <- newElem "div"
+  setChildren buttonDiv [diffButton, drawButton]
+
+  setChildren documentBody [canvas, expElem, scaleElem, buttonDiv]
+
+  focus expInput
+  select expInput
+
   Just can <- getCanvas canvas
-  onEvent drawBtn   OnClick $ \_ _  -> readAndDraw expElem scaElem can
-  onEvent expElem   OnKeyUp $ \code -> when (code == 13) $ readAndDraw expElem scaElem can
-  onEvent diffBtn   OnClick $ \_ _  -> readAndDifferentiate expElem scaElem can
+  onEvent drawButton  OnClick $ \_ _  -> readAndDraw expInput scaleInput can
+  onEvent expInput    OnKeyUp $ \code -> when (code == 13) $ readAndDraw expInput scaleInput can
+  onEvent diffButton  OnClick $ \_ _  -> readAndDifferentiate expInput scaleInput can
+
+newCanvas :: Int -> Int -> IO Elem
+newCanvas width height = do
+  canvas <- newElem "canvas"
+  setProp canvas "width" (show width)
+  setProp canvas "height" (show height)
+  setStyle canvas "margin" "auto"
+  setStyle canvas "display" "block"
+  setStyle canvas "background-color" "white"
+  return canvas
+
+newInput :: String -> String -> IO (Elem, Elem)
+newInput value label = do
+  inputElem <- newElem "input"
+  setProp inputElem "type" "text"
+  setProp inputElem "value" value
+  setStyle inputElem "width" "240px"
+  setStyle inputElem "height" "25px"
+  setStyle inputElem "line-height" "25px"
+  setStyle inputElem "border" "0"
+  setStyle inputElem "padding" "0"
+  setStyle inputElem "outline" "none"
+  setStyle inputElem "font-family" "arial"
+  setStyle inputElem "font-size" "14px"
+
+  labelElem <- newElem "span"
+  setProp labelElem "innerHTML" label
+  setStyle labelElem "display" "inline-block"
+  setStyle labelElem "width" "40px"
+  setStyle labelElem "text-align" "left"
+  setStyle labelElem "font-family" "arial"
+  setStyle labelElem "font-size" "14px"
+  setStyle labelElem "color" "#bbbbbb"
+
+  div <- newElem "div"
+  setStyle div "display" "inline-block"
+  setStyle div "width" "280px"
+  setStyle div "margin-top" "10px"
+  setStyle div "padding" "0px 10px"
+  setStyle div "background-color" "white"
+
+  div' <- newElem "div"
+
+  setChildren div [labelElem, inputElem]
+  setChildren div' [div]
+
+  return (div', inputElem)
+
+newButton :: String -> IO Elem
+newButton value = do
+  button <- newElem "div"
+  setProp button "innerText" value
+  setStyle button "display" "inline-block"
+  setStyle button "margin" "10px"
+  setStyle button "height" "25px"
+  setStyle button "line-height" "25px"
+  setStyle button "padding" "0px 10px"
+  setStyle button "cursor" "pointer"
+  setStyle button "background-color" "black"
+  setStyle button "font-family" "arial"
+  setStyle button "color" "white"
+  return button
+
+select :: Elem -> IO ()
+select = ffi $ toJSStr "(function(e) {e.select();})"
